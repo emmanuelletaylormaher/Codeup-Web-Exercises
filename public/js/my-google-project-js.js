f955ea8827051bd42b8596b955762c88528f43bc
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-06-27 09:36:05 -0500

    Made some changes to the calculator project. Created and worked on files dealing with Google Maps APIs.

---
+++ public/js/my-google-project-js.js
@@ -0,0 +1,42 @@
+(function(){
+	"use strict";
+
+	var mapOptions = {
+		zoom: 17,
+
+		center: {
+			lat: 29.526755,
+			lng: -98.566169,
+		}
+	};
+
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	var yellowfish = {lat: 29.526755, lng: -98.566169};
+
+	var marker = new google.maps.Marker({
+		position: yellowfish,
+		map: map,
+		animation: google.maps.Animation.DROP
+	});
+	
+	marker.addListener('click', toggleBounce);
+
+	function toggleBounce() {
+		if (marker.getAnimation() !== null) {
+			marker.setAnimation(null);
+		} else {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+			setTimeout(function(){ marker.setAnimation(null);}, 2750);
+		}
+	}
+
+	var infowindow = new google.maps.InfoWindow({
+		content: "Yellowfish Sushi: A neat Japanese-Mexican fusion restaurant."
+	});
+
+	marker.addListener('click', function(){
+		infowindow.open(map,marker);
+	});
+
+})();
