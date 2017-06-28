0aadbddb33f48f6e0202ea446ab503b8e9b38fce
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-06-28 17:47:55 -0500

    Did more exercises in jquery_exercises.js involving mouse interactions.

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -3,3 +3,19 @@
 $(document).ready(function () {
 	alert("The DOM has finished loading!");
 });
+
+$('h1').click(function(){
+	$('h1').css("background-color", "green");
+});
+
+$('p').dblclick(function() {
+	$('p').css("font-size", "18px");
+});
+
+$("li").hover(function (){
+		$(this).css("color", "red");
+});
+
+$("li").mouseleave(function(){
+	$(this).css("color", "white");
+});
