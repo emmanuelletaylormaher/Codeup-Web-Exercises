0555b0d351d26bb20a9a3f53947ca46e48c32afa
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-07-31 16:59:37 -0500

    Added counter.php to file

---
+++ public/counter.php
@@ -0,0 +1,28 @@
+<?php
+
+function pageController(){
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Counter!</title>
+</head>
+<body>
+	<h1></h1>
+
+	<form method="GET" action="https://codeup.dev/counter.php/">
+		<input type="text" name="q" value="" placeholder="Counter!!!!">
+		<button type="submit">Count UP</button>
+	</form>
+	<br>
+	<form method="GET" action="https://codeup.dev/counter.php/">
+		<input type="text" name="q" value="" placeholder="Counter!!!!">
+		<button type="submit">Count Down</button>
+	</form>
+
+</body>
+</html>
