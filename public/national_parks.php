5d64d9880325b574029a82fc86711370f20e357a
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-11 12:00:33 -0500

    Made table in HTML that presents fetched info from SQL database

--- public/national_parks.php
+++ public/national_parks.php
@@ -2,14 +2,17 @@
 
 require_once __DIR__ . "/../db_connect.php";
 
-$query = "SELECT * FROM national_parks LIMIT 4";
+$offset = 0;
+$query = "SELECT * FROM national_parks LIMIT 4 OFFSET " . $offset;
 $stmt = $dbc->query($query);
 var_dump($stmt);
 
-do {
-	$result = $stmt->fetch();
-	print_r($result);
-} while ($result);
+$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+print_r($parks);
+
+function pageController(){
+
+}
 
 ?>
 
@@ -21,9 +24,35 @@ do {
 <body>
 	<h1>These are some National Parks!</h1>
 
+	<table>
+		<tr>
+			<th>Name</th>
+			<th>Location</th>
+			<th>Date Established</th>
+			<th>Area in Acres</th>
+		</tr>
+
+		<?php foreach ($parks as $park): ?>
+			<tr>
+				<td> <?= $park["name"]; ?></td>
+				<td> <?= $park["location"]; ?></td>
+				<td> <?= $park["date_established"]; ?></td>
+				<td> <?= $park["area_in_acres"]; ?></td>
+			</tr>
+		<?php endforeach ?>
+	</table>
+
 
 	<!-- go previous -->
+	<form method="GET" action="national_parks.php">
+		<input type="hidden" name="navigate" value="previous">
+		<button>previous</button>
+	</form>
 	<!-- go next -->
+	<form method="GET" action="national_parks.php">
+		<input type="hidden" name="navigate" value="next">
+		<button>next</button>
+	</form>
 
 </body>
 </html>
