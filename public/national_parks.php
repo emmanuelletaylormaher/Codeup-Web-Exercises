1557ba936efeb052b9d55fb839d3349cf722ac5b
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-11 14:03:59 -0500

    Added pagination functionality

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,18 +1,32 @@
 <?php
 
 require_once __DIR__ . "/../db_connect.php";
+require_once "../Input.php";
 
-$offset = 0;
-$query = "SELECT * FROM national_parks LIMIT 4 OFFSET " . $offset;
-$stmt = $dbc->query($query);
-var_dump($stmt);
 
-$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($parks);
 
-function pageController(){
+function pageController($dbc){
+	$page=Input::getNumeric("page", 1);
+	$offset = (($page - 1)*4);
+	$query = "SELECT * FROM national_parks LIMIT 4 OFFSET " . $offset;
+	$stmt = $dbc->query($query);
 
+	$parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$navigate=Input::get("navigate");
+
+
+	$data = array(
+			"navigate" => $navigate,
+			"parks" => $parks,
+			"page" => $page
+	);
+
+	return $data;
 }
+
+// offset = (n-1)*4
+
+extract(pageController($dbc));
 
 ?>
 
@@ -45,13 +59,13 @@ function pageController(){
 
 	<!-- go previous -->
 	<form method="GET" action="national_parks.php">
-		<input type="hidden" name="navigate" value="previous">
-		<button>previous</button>
+		<input type="hidden" name="page" value=<?= $page - 1 ?>>
+		<button <?php if ($page == 1) {?> style="display:none;" <?php }else {?> style="display:initial;" <?php } ?> >previous</button>
 	</form>
 	<!-- go next -->
 	<form method="GET" action="national_parks.php">
-		<input type="hidden" name="navigate" value="next">
-		<button>next</button>
+		<input type="hidden" name="page" value=<?= $page + 1 ?>>
+		<button <?php if ($page == 15) {?> style="display:none;" <?php }else {?> style="display:initial;" <?php } ?>>next</button>
 	</form>
 
 </body>
