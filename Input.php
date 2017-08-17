cb172a1a5b33b6c6caddc2a3d87cfd2f5993d617
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-17 09:36:56 -0500

    Added new methods

--- Input.php
+++ Input.php
@@ -47,6 +47,43 @@ class Input
         return htmlspecialchars(strip_tags($input));
     }
 
+    public static function getString ($key)
+    {
+        $value = self::get($key, $default);
+
+        if(!is_string($value) || is_numeric($value)) {
+            throw new Exception ("Value must be a string!");
+        } else if (empty($value)) {
+            throw new Exception("Cannot be empty");
+            
+        }
+
+        return $value;
+    }
+
+    public static function getNumber ($key)
+    {
+        $input = self::get($key);
+        if (!is_numeric($input)){
+            throw new Exception("Must be a number");
+        } elseif (empty($input)) {
+            threw new Exception("Cannot be empty");
+        }
+        return input;
+    }
+
+    public static function getDate($key){
+        $input = self::get($key);
+        if (!is_numeric(strtotime($input))) {
+            threw new Exception("Must be a valid date");
+        } else {
+            $date = new DateTime();
+            $date->setTimestamp(strtotime($input));
+            $date->setTimezone(new DateTimeZone("America/Chicago"));
+        }
+        return $date;
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     //                      DO NOT EDIT ANYTHING BELOW!!                     //
     // The Input class should not ever be instantiated, so we prevent the    //
